31d79ebe1fa03548cbc72e7b5759115b4bbd7e5c
fxf28 <faizfjr666@gmail.com>
2025-01-13 17:46:51 +0700

    sudah mas haidar kurang huruf s saja tadi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     @include('partials.hero')
-    {{-- @include('partials.article', ['posts' => $posts]) --}}
-    @include('partials.articles', ['posts' => $posts])
+    @include('partials.article-haidar', ['posts' => $posts])
+    {{-- @include('partials.articles', ['posts' => $posts]) --}}
     @include('partials.about')
     @include('partials.contact')
 @endsection
